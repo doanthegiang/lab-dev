2465ddbf05befbe9bc4a5f05b3d21f7256efeca4
DESKTOP-1M107Q0\giang <giangdoanthe@gmail.com>
2022-08-31 15:01:45 +0700

    no message

--- docker/cron/renew_certs.sh
+++ docker/cron/renew_certs.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-set -e
+cd /var/www
 
 echo "Generating dummy ceritificate for bon.com"
 # mkdir -p "/etc/nginx/sites/ssl/dummy/bon.com"
-printf "[dn]\nCN=bon.com\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:bon.com, DNS:www.bon.com\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" > openssl.cnf
-openssl req -x509 -out "/var/nginx/docker/nginx/nginx-selfsigned.crt" -keyout "/var/nginx/docker/nginx/nginx-selfsigned.key" \
-    -newkey rsa:2048 -nodes -sha256 \
-    -subj "/CN=bon.com" -extensions EXT -config openssl.cnf
-rm -f openssl.cnf
-  
+
+openssl req -x509 -out "/var/www/docker/nginx/nginx-selfsigned.crt" -keyout "/var/www/docker/nginx/nginx-selfsigned.key" \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=bon.com' -extensions EXT -config <( \
+   printf "[dn]\nCN=bon.com\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:bon.com\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+# docker compose exec --no-TTY nginx nginx -s reload
